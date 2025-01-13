3a35765ae6907acf5c3b9c2d27eecce905be0e51
Jakob <sokolov.yakov@inbox.ru>
2025-01-13 14:35:40 +0500

    10.2.1

--- apps/blog/admin.py
+++ apps/blog/admin.py
@@ -1,5 +1,12 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, Category
+from mptt.admin import DraggableMPTTAdmin
+
+@admin.register(Category)
+class CategoryAdmin(DraggableMPTTAdmin):
+    """
+    Админ-панели модели другой категории
+    """
+    prepopulated_fields = {'slug': ('title',)}
 
 admin.site.register(Post)
-
